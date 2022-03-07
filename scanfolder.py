bb1be09d9fb56ed0d80063443df704522a268481
ShardIce <48800677+ShardIce@users.noreply.github.com>
2022-03-08 02:18:33 +0300

    Update scanfolder.py

--- scanfolder.py
+++ scanfolder.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 
-FILES_INPUT = 'C:/Users/admin/Desktop/python/rookie' # Входные файлы
-FILES_OUTPUT = 'C:/Users/admin/Desktop/python/' # Выходные файлы
+FILES_INPUT = 'C:/Users/admin/Desktop/python/rookie'
+FILES_OUTPUT = 'C:/Users/admin/Desktop/python/rookie'
 
 for filename in os.listdir(FILES_INPUT):
-     filepath = os.path.join(FILES_INPUT, filename) #Путь и файл
+     filepath = os.path.join(FILES_INPUT, filename) 
      base, ext = os.path.splitext(filepath) # Создаем папку назначения
-     os.mkdir(base) # Создаем папку как название файла
+     os.makedirs(base, exist_ok=True) # Создаем папку как название файла
      path = shutil.move(filepath, base) # Перенесем файл внутрь папки
      print(path)
