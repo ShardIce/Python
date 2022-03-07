5075925ea0636a2feb46ce6f885f7573af1e8bd0
ShardIce <48800677+ShardIce@users.noreply.github.com>
2022-03-07 09:05:02 +0300

    Create scanfolder.py

---
+++ scanfolder.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+FILES_INPUT = 'C:\\Users\\admin\\Desktop\\python\\rookie' # Входные файлы
+FILES_OUTPUT = 'C:\\Users\\admin\\Desktop\\python\\' # Входные файлы
+
+for dirpath, dirnames, filenames in os.walk(FILES_INPUT):
+    for filename in filenames:
+        if os.listdir(FILES_INPUT):
+            if not os.path.isdir(filename):
+                for filename in filenames:
+                    os.mkdir(os.path.splitext(filename)[0])
+                    shutil.move(os.path.join(FILES_INPUT, filename), FILES_OUTPUT)
+        else:
+                    print("НЕТ НИЧЕГО")
