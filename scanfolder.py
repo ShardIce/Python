211d905c9f4a6f672deb008d90350c73ca174dca
ShardIce <48800677+ShardIce@users.noreply.github.com>
2022-03-07 20:53:27 +0300

    Update scanfolder.py

--- scanfolder.py
+++ scanfolder.py
@@ -1,5 +1,8 @@
+#import glob
 import shutil
 import os
+import pathlib
+from pathlib import Path
 
 FILES_INPUT = 'C:\\Users\\admin\\Desktop\\python\\rookie' # Входные файлы
 FILES_OUTPUT = 'C:\\Users\\admin\\Desktop\\python\\' # Выходные файлы
@@ -10,6 +13,8 @@ for dirpath, dirnames, filenames in os.walk(FILES_INPUT):
             if not os.path.isdir(filename):
                 for filename in filenames:
                     os.mkdir(os.path.splitext(filename)[0])
-                    shutil.move(os.path.join(FILES_INPUT, filename), FILES_OUTPUT)
+                    print(Path(FILES_OUTPUT, os.path.splitext(filename)[0]))
+                    move_files = Path(FILES_OUTPUT, os.path.splitext(filename)[0])
+                    shutil.move(move_files)
         else:
                     print("НЕТ НИЧЕГО")
