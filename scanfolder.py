172c657d8813f060d24b042c31dc84bbb1c3293c
ShardIce <48800677+ShardIce@users.noreply.github.com>
2022-03-07 23:31:35 +0300

    Update scanfolder.py

--- scanfolder.py
+++ scanfolder.py
@@ -7,7 +7,6 @@ FILES_OUTPUT = 'C:/Users/admin/Desktop/python/' # Выходные файлы
 dirpath = FILES_INPUT #Путь к файлам
 for filename in os.listdir(dirpath):
      filepath = os.path.join(dirpath, filename) #Путь и файл
-     dirpath, filename = os.path.split(filepath) #Путь, файлы
      base, ext = os.path.splitext(filepath) # Создаем папку назначения
      os.mkdir(base)
      path = shutil.move(filepath, base) # Перенесем файл внутрь папки
