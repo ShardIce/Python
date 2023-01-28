7a992b56fbf6a728b7fb554ca24c8bd8d3250431
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-28 15:22:19 -0800

    Update scanfolder.py

--- scanfolder.py
+++ scanfolder.py
@@ -5,8 +5,11 @@ FILES_INPUT = 'C:/Users/admin/Desktop/python/rookie'
 FILES_OUTPUT = 'C:/Users/admin/Desktop/python/rookie'
 
 for filename in os.listdir(FILES_INPUT):
-     filepath = os.path.join(FILES_INPUT, filename) 
+    filepath = os.path.join(FILES_INPUT, filename)
+    if os.path.isfile(filepath): # Проверяем файл или папка
      base, ext = os.path.splitext(filepath) # Создаем папку назначения
      os.makedirs(base, exist_ok=True) # Создаем папку как название файла
      path = shutil.move(filepath, base) # Перенесем файл внутрь папки
      print(path)
+    else:
+     print('Это папка:', filename)
